3d4eeb08cb690b40a14d48206e8d3e439544e0dc
aqil zaki <132360038+aqilzaki@users.noreply.github.com>
2026-09-11 08:28:29 +0700

    Initial Commit

---
+++ HIGHWAY/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HIGHWAY
+//
+//  Created by muhammad aqil zaki on 11/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HIGHWAY/HIGHWAYApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIGHWAYApp.swift
+//  HIGHWAY
+//
+//  Created by muhammad aqil zaki on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct HIGHWAYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
